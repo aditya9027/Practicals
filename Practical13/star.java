3910ca90a38d26c6476398a521f68fdb226bdf9d
Developer <adityas9027@gmail.com>
2026-01-27 17:06:49 +0530

    Table java code

--- Practical13/star.java
+++ Practical13/star.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class star{
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter any number for Printing the Table : " );
+        System.out.print("Enter any number for Printing the Table  : " );
         int num = scanner.nextInt();
 
         for(int i=1; i <= 10; i++){
