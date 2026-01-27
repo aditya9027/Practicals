e665675d78b13f6ab00c4f412b4d38d3aa02bf75
Developer <adityas9027@gmail.com>
2026-01-27 17:04:01 +0530

    Table java code

---
+++ Practical13/star.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class star{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter any number for Printing the Table : " );
+        int num = scanner.nextInt();
+
+        for(int i=1; i <= 10; i++){
+            System.out.println(num + " * " + i  +" = "+i * num);
+        }
+    }
+}
