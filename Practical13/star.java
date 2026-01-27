6c1d3072078d6072ea508c4a141366eeb112f2e6
Developer <adityas9027@gmail.com>
2026-01-27 17:12:07 +0530

    code update

--- Practical13/star.java
+++ Practical13/star.java
@@ -7,8 +7,8 @@ class star{
         int num = scanner.nextInt();
 
         for(int i=1; i <= 10; i++){
-            // System.out.println(num + " * " + i  +" = "+i * num);
-            System.out.println(i * num);
+            // System.out.println(i * num);
+            System.out.println(num + " * " + i  +" = "+i * num);
         }
     }
 }
