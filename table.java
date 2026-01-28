3f90f85b83f1366268d8dc3eb6dfb331233e3905
Developer <adityas9027@gmail.com>
2026-01-28 10:20:10 +0530

    code update

--- table.java
+++ table.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-class star{
+class table{
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter any number for Printing the Table  : " );
-        // int num = scanner.nextInt();
         int num = 5;
+        System.out.print("Enter any number for Printing the Table  : " + num);
+        // int num = scanner.nextInt();
 
         for(int i=1; i <= 10; i++){
             System.out.println(num + " * " + i  +" = "+i * num);
