38e6ba1958307c7091b76da201fa7eb563812f32
aditya9027 <78259475+aditya9027@users.noreply.github.com>
2026-01-28 17:57:47 +0530

    Change table number from 5 to 15

--- table.java
+++ table.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class table{
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
-        int num = 5;
+        int num = 15;
         System.out.print("Enter any number for Printing the Table  : ");
         // int num = scanner.nextInt();
 
